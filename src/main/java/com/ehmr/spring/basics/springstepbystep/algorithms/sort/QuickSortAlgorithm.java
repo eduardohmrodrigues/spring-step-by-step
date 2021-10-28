da20b6467836b41f62dbf919d086b320348ad569
Eduardo Rodrigues <eduardohmrodrigues@gmail.com>
2021-10-28 00:59:13 -0300

    Set quick sort as primary injection for spring

--- src/main/java/com/ehmr/spring/basics/springstepbystep/algorithms/sort/QuickSortAlgorithm.java
+++ src/main/java/com/ehmr/spring/basics/springstepbystep/algorithms/sort/QuickSortAlgorithm.java
@@ -1,7 +1,12 @@
 package com.ehmr.spring.basics.springstepbystep.algorithms.sort;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
 import java.util.Arrays;
 
+@Component
+@Primary
 public class QuickSortAlgorithm implements SortAlgorithm {
     public int[] sort(int[] arr) {
         // Implement quick sort algorithm instead of this default impl
